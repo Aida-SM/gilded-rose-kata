ba26af7c59a29690928ba2208a550b1d30171949
Aida Sabaghmoghadam <aida.sabbagh@gmail.com>
2019-02-26 11:34:19 -0500

    Change the name of test updateItem method and creat set up method for Item

--- src/test/java/com/gildedrose/UpdatesItemQualityTest.java
+++ src/test/java/com/gildedrose/UpdatesItemQualityTest.java
@@ -1,17 +1,28 @@
 package com.gildedrose;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
 
 public class UpdatesItemQualityTest {
+    Item item;
+
+    @Before
+    public void setUp() throws Exception {
+         item = new Item("foo", 0, 0);
+    }
+
     @Test
-    public void shouldReturnUpdateItem() {
+    public void updateItem_shouldReturnUpdateItem() {
 
         UpdatesItemQuality updatesItemQuality = new UpdatesItemQuality();
-        Item item = new Item("foo", 0, 0);
-         updatesItemQuality.updateItem(item);
-        Assert.assertEquals(item,updatesItemQuality.updateItem(item));
+       Assert.assertEquals(item,updatesItemQuality.updateItem(item));
     }
-}
+
+
+
+
+    }
