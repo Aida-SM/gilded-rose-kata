05447940d729f8222af4aa9f6a327f4c37c67130
Aida Sabaghmoghadam <aida.sabbagh@gmail.com>
2019-02-26 21:36:36 -0500

    Change the return value of update method in UpdatesSpecialEventItem class(not actual)

--- src/main/java/com/gildedrose/UpdatesSpecialEventItem.java
+++ src/main/java/com/gildedrose/UpdatesSpecialEventItem.java
@@ -3,6 +3,7 @@ package com.gildedrose;
 public class UpdatesSpecialEventItem implements UpdatesItem{
     @Override
     public Item update(Item item) {
-        return item;
+        int newQuality = 2;
+        return new Item(item.name, item.sellIn - 1, Math.min(newQuality, 50));
     }
 }
