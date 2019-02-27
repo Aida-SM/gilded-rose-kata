90cf22d67e462cb60d35a8a2c7c38f0e39ab8162
Aida Sabaghmoghadam <aida.sabbagh@gmail.com>
2019-02-26 21:23:46 -0500

    Change the return value of update method of UpdatesRegularItem class

--- src/main/java/com/gildedrose/UpdatesRegularItem.java
+++ src/main/java/com/gildedrose/UpdatesRegularItem.java
@@ -3,6 +3,7 @@ package com.gildedrose;
 public class UpdatesRegularItem implements UpdatesItem {
     @Override
     public Item update(Item item) {
-        return item;
+        return new Item(item.name, item.sellIn - 1, new QualityAdjuster().adjust(item, - 1));
+
     }
 }
